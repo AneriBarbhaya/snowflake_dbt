37201576ac65f31c46e4e59c12210d5100902768
Aneri Barbhaya <aneri.barbhaya4@gmail.com>
2026-02-22 17:45:43 +0000

    added customer lifetime value to dimension customer

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,7 +1,32 @@
-select o.order_id, 
-o.customer_id, 
-p.amount
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
 
-from {{ ref('stg_jaffle_shop__orders') }} o
-left join {{ ref('stg_stripe__payments') }} p
-on o.order_id = p.orderid
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by order_id
+),
+
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders 
+    left join order_payments 
+    on orders.order_id = order_payments.order_id
+)
+
+select * from final
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,10 @@
 select
     id as payment_id,
-    orderid,
+    orderid as order_id,
     paymentmethod as payment_method, 
     status, 
-    amount, 
-    created
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
 
 from RAW.STRIPE.PAYMENT

84a0374d3c931e8b43ab4c0824c9bd84298c5ec7
Aneri Barbhaya <aneri.barbhaya4@gmail.com>
2026-02-23 05:52:27 +0000

    adding incremental strategy to fct_orders

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 with orders as  (
     select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
@@ -30,3 +36,7 @@ final as (
 )
 
 select * from final
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where order_date > (select max(order_date) from {{ this }}) 
+{% endif %}
